cc8582776af6774f9c62a844d22261a42c95a55e
Sandsunset <sejoon0606@gmail.com>
2021-08-05 17:27:21 +0900

    feat: main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import icon from './icon.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Introduce extends Component {
+  render() {
+    return (
+      <div>
+        <div className="introduce">
+          <h1 id="m_title">Check Today's Zoom Schedule</h1>
+          <div id="m_icon_div">
+            <img src={icon} alt="icon"/>
+          </div>
+  
+          <button id="m_get_github" onclick="location.href = 'https\://github.com/Classto'">
+            <i className="fab fa-github fa-3x"></i>
+          </button>
+  
+          <button id="m_get_start" onClick="location.href='/editor'">
+            <p id="m_get_start_text">Get Started</p>
+            <i id="m_get_start_icon" className="fas fa-angle-right fa-3x"></i>
+          </button>
+        </div>
+        <p id="copyright">Copyright 2021. ClassNameTo all rights reserved.</p>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default Introduce;
