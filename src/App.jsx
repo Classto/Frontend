7e76e42f2b868e5c6ca0d9d05a5bc03fd08af055
Sandsunset <sejoon0606@gmail.com>
2021-08-14 22:49:44 +0900

    feat: move to editor page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Component } from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
 import icon from './icon.svg';
 import './App.css';
+import Editor from './editor.jsx';
 
 class Introduce extends Component {
   render() {
@@ -11,15 +13,16 @@ class Introduce extends Component {
           <div id="m_icon_div">
             <img src={icon} alt="icon"/>
           </div>
-  
           <button id="m_get_github" onclick="location.href = 'https\://github.com/Classto'">
             <i className="fab fa-github fa-3x"></i>
           </button>
-  
-          <button id="m_get_start" onClick="location.href='/editor'">
-            <p id="m_get_start_text">Get Started</p>
-            <i id="m_get_start_icon" className="fas fa-angle-right fa-3x"></i>
-          </button>
+          <BrowserRouter>
+            <Link to="/editor" id="m_get_start">
+              <p id="m_get_start_text">Get Started</p>
+              <i id="m_get_start_icon" className="fas fa-angle-right fa-3x"></i>
+            </Link>
+            <Route path="/editor" component={Editor}/>
+          </BrowserRouter>
         </div>
         <p id="copyright">Copyright 2021. ClassNameTo all rights reserved.</p>
       </div>
