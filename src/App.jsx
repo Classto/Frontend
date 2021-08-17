28b26a6e17b035f056516a3026fccb9a2030996c
Sandsunset <sejoon0606@gmail.com>
2021-08-17 16:06:04 +0900

    fix: fix bug display multiple router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
 import { Component } from 'react';
-import {BrowserRouter, Route, Link} from 'react-router-dom';
-import icon from './icon.svg';
-import './App.css';
+import { Route, BrowserRouter } from 'react-router-dom';
 import Editor from './editor.jsx';
+import Introduce from './introduce.jsx';
 
-class Introduce extends Component {
+class App extends Component {
   render() {
     return (
       <div>
-        <div className="introduce">
-          <h1 id="m_title">Check Today's Zoom Schedule</h1>
-          <div id="m_icon_div">
-            <img src={icon} alt="icon"/>
-          </div>
-          <button id="m_get_github" onclick="location.href = 'https\://github.com/Classto'">
-            <i className="fab fa-github fa-3x"></i>
-          </button>
-          <BrowserRouter>
-            <Link to="/editor" id="m_get_start">
-              <p id="m_get_start_text">Get Started</p>
-              <i id="m_get_start_icon" className="fas fa-angle-right fa-3x"></i>
-            </Link>
-            <Route path="/editor" component={Editor}/>
-          </BrowserRouter>
-        </div>
-        <p id="copyright">Copyright 2021. ClassNameTo all rights reserved.</p>
+        <BrowserRouter>
+          <Route exact path="/" component={Introduce}/>
+          <Route path="/editor" component={Editor}/>
+        </BrowserRouter>
       </div>
-    );
+    )
   }
 }
 
-export default Introduce;
+export default App;
