5b0329c7fd66ef0a9debfb935deae5ed51037f66
ERRrOR404 <error99998@gmail.com>
2021-09-17 06:39:15 +0900

    feat: add Link tag on menus

--- src/editor/menu.jsx
+++ src/editor/menu.jsx
@@ -1,11 +1,12 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './menu.css';
 
 class Menu extends Component {
   render() {
     const categorys = ['sample', 'school', 'game', 'this is very long category']
     const menu = categorys.map((category) => 
-      <li key={ category }>{ category }</li>
+      <li key={ category }><Link to={ "/editor/" + category }>{ category }</Link></li>
     )
     return(
       <div id="Menu">
