b0fec584c8f0bc0109bd32499fb4f391cf55f0e1
Sandsunset <sejoon0606@gmail.com>
2021-09-16 11:37:22 +0900

    chore(menu): change file name:

---
+++ src/editor/menu.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import './menu.css';
+
+class Layout extends Component {
+    render() {
+        return(
+            <div>
+                <ul>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+
+
+
+// 님 팡
+export default Layout;
