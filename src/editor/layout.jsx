de8f405b8e469f6c6ddd4dfeb381e315ea44d8e3
ERRrOR404 <error99998@gmail.com>
2021-09-16 11:26:44 +0900

    feat: add code

--- src/editor/layout.jsx
+++ src/editor/layout.jsx
@@ -2,7 +2,13 @@ import { Component } from 'react';
 import './layout.css';
 
 class Layout extends Component {
-
+    render() {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
 }
 
 export default Layout;
