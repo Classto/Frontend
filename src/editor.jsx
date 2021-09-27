606423355fef1233307170d2a363c25e27ab28ee
Sandsunset <sejoon0606@gmail.com>
2021-09-27 09:55:06 +0900

    refactor(editor): toggle menu

--- src/editor.jsx
+++ src/editor.jsx
@@ -8,26 +8,43 @@ class Editor extends Component {
   constructor() {
     super()
     this.state = {
-      is_menu_open : false
+      toggle_menu : 'none'
     }
   }
   componentDidMount() {
     document.title = 'Classto - Editor';
 
-    let isMenuOpen = this.state.is_menu_open
+    // let isMenuOpen = this.state.is_menu_open
 
-    document.getElementById('Menu').style.display = 'none';
+    // document.getElementById('Menu').style.display = 'none';
 
-    document.getElementById('category').addEventListener('click', (event) => {
-      const menuPnl = document.getElementById('Menu');
-      if (isMenuOpen) {
-        menuPnl.style.display = 'none';
-        isMenuOpen = false;
-      } else if (!isMenuOpen) {
-        menuPnl.style.display = 'block';
-        isMenuOpen = true;
-      }
-    });
+    // document.getElementById('category').addEventListener('click', (event) => {
+    //   const menuPnl = document.getElementById('Menu');
+    //   if (isMenuOpen) {
+    //     menuPnl.style.display = 'none';
+    //     isMenuOpen = false;
+    //   } else if (!isMenuOpen) {
+    //     menuPnl.style.display = 'block';
+    //     isMenuOpen = true;
+    //   }
+    // });
+  }
+
+  toggle_menu() {
+    switch(this.state.toggle_menu) {
+      default:
+        break
+      case 'none':
+        this.setState({
+          toggle_menu: 'block',
+        })
+        break
+      case 'block':
+        this.setState({
+          toggle_menu: 'none',
+        })
+        break
+    }
   }
 
   render() {
@@ -44,11 +61,13 @@ class Editor extends Component {
           <div></div>
         </button> */}
         <div id="category-div">
-          <div id="category">
+          <div id="category" onClick={ this.toggle_menu.bind(this) }>
             <p>{ params['category']}</p>
             <i className="fas fa-chevron-down fa-2x"></i>
           </div>
-          <Menu/>
+          <div style={{ display: this.state.toggle_menu }}>
+            <Menu/>
+          </div>
         </div>
         <Panel/>
       </div>
