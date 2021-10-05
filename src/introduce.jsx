4c17591b16a4b6a8fc3aeca2064aad5cefa301b2
ERRrOR404 <error99998@gmail.com>
2021-10-05 09:19:57 +0900

    faet: done introduce page

--- src/introduce.jsx
+++ src/introduce.jsx
@@ -8,11 +8,15 @@ class Introduce extends Component {
     document.title = 'Classto';
   }
 
+  move_to_github() {
+    window.location.href = "https://github.com/Classto"
+  }
+
   render() {
     let editor_link = window.localStorage.getItem("recent_editor")
     return (
       <div id="introduce">
-        <h5 id="main-title">Classto</h5>
+        <h1 id="main-title">Classto</h1>
         <h1 id="main-description">Join Your Meeting Easily</h1>
         <Link id="main-link" to={`editor/${editor_link}`}>
           <div id="main-editor">
@@ -21,6 +25,26 @@ class Introduce extends Component {
           </div>
         </Link>
         <img id="main-landscape" src={landscape} alt="icon"/>
+        <nav id="main-usage">
+          <div id="about">
+            <h1>About Classto</h1>
+            <p>Classto is open-source project to join meeting more easily.<br/>You can view our GitHub repository on <span onClick={ this.move_to_github }>here</span>.</p>
+          </div>
+          <div id="usage">
+            <h1>Usage</h1>
+            <p>
+              Click<br/>Add your meetings, and just wait on time. 
+              <Link id="main-link" to={`editor/${editor_link}`}>
+                <div id="main-usage-editor">
+                  <i className="fas fa-calendar-day"></i>
+                  <p>Open Editor</p>
+                </div>
+              </Link>
+              <span>to start to use Classto.</span>
+            </p>
+          </div>
+        </nav>
+        <p id="copyright">Copyright ©2021 Zoom Video Communications, Inc. All rights reserved.</p>
       </div>
     );
   }
