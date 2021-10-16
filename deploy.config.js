7e8888a92a9a9e973d2e518da7e0267fd051f145
Sandsunset <sejoon0606@gmail.com>
2021-10-16 23:30:19 +0900

    chore: fix deploy code

--- deploy.config.js
+++ deploy.config.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'classto',
     script: 'npx',
     interpreter: 'none',
-    args: 'serve public/ -p 80', 
+    args: 'serve public/ -p 8080', 
     instances : '1',
     exec_mode : 'cluster'
   };
