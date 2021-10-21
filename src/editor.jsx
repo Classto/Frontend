5c061670aefde17a2f17088c8ff80257fd950a6d
Sandsunset <sejoon0606@gmail.com>
2021-10-21 20:14:39 +0900

    rollback

--- src/editor.jsx
+++ src/editor.jsx
@@ -8,7 +8,7 @@ class Editor extends Component {
   constructor() {
     super()
     this.state = {
-      toggle_category_menu : 'none'
+      toggle_menu : 'none'
     }
   }
 
@@ -27,54 +27,58 @@ class Editor extends Component {
   enter_zoom() {
     const current_time = new Date()
     const reserved_meetings = JSON.parse(localStorage.getItem('meetings'))[this.params['category']]
-    let hour = `${current_time.getHours()}`.length === 1 ? `0${current_time.getHours()}` : `${current_time.getHours()}`
-    let minute = `${current_time.getMinutes()}`.length === 1 ? `0${current_time.getMinutes()}` : `${current_time.getMinutes()}`
+    
     reserved_meetings.forEach(meeting => {
-      if (meeting.time === `${hour}:${minute}` && meeting["repeating-days"].includes(current_time.getDay().toString()))
-        window.location.href = `zoommtg://zoom.us/join?action=join&confno=${meeting.id}&pwd=${meeting.pwd}&uname=${meeting.nickname}`
+      if (meeting.time === `${current_time.getHours()}:${current_time.getMinutes()}` && meeting["repeating-days"].includes(current_time.getDay().toString())) {
+        try {
+          window.location.href = `zoommtg://zoom.us/join?action=join&confno=${meeting.id}&pwd=${meeting.pwd}&uname=${meeting.nickname}`
+        } catch(errror) {
+          //줌이 설치되어있지 않을 때
+        }
+      }
     });
+
+    console.log(current_time.getHours(), current_time.getMinutes())
   }
 
-  toggle_category_menu() {
-    switch(this.state.toggle_category_menu) {
+  toggle_menu() {
+    switch(this.state.toggle_menu) {
       default:
         break
       case 'none':
         this.setState({
-          toggle_category_menu: 'block',
+          toggle_menu: 'block',
         })
         break
       case 'block':
         this.setState({
-          toggle_category_menu: 'none',
+          toggle_menu: 'none',
         })
         break
     }
   }
 
   render() {
-    const { params } = this.props.match //여기 코드 너무 엉망이어서 나중에 최적화 할게요 일단 놔두셈
+    const { params } = this.props.match
     this.params = params
-    let meetings = []
-    for (let i = 0; i < JSON.parse(window.localStorage.getItem("meetings"))[this.params["category"]].length; i++) {
-      if (JSON.parse(window.localStorage.getItem("meetings"))[this.params["category"]][i].category === this.params['category']) {
-        meetings.push(JSON.parse(window.localStorage.getItem("meetings"))[this.params["category"]][i])
-      }
-    }
+    const meetings = JSON.parse(window.localStorage.getItem("meetings"))[params['category']]
+    const category = params["category"]
+    console.log(typeof category)
+
     return (
       <div>
         <hr id="ct_hr"></hr>
         <div id="sch_div">
-          <Schedule meetings={ meetings } category={ this.params["category"] }/>
+          <Schedule meetings={ meetings } category={ category }/>
         </div>
         <div id="category-div">
-          <div id="category" onClick={ this.toggle_category_menu.bind(this) }>
+          <div id="category" onClick={ this.toggle_menu.bind(this) }>
             <div>
               <p>{ params['category'] }</p>
             </div>
             <i className="fas fa-chevron-down fa-2x"></i>
           </div>
-          <div style={{ display: this.state.toggle_category_menu }}>
+          <div style={{ display: this.state.toggle_menu }}>
             <CategoryMenu/>
           </div>
         </div>
